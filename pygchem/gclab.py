41c234a155d972d9d2c19ffd1e06894e65b788c2
Benoît Bovy <bbovy@ulg.ac.be>
2014-02-19 14:34:44 +0100

    matplotlib's pylab or mayavi's mlab like interface (in development)

---
+++ pygchem/gclab.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# module gclab
+# parts of pygchem (Python interface for GEOS-Chem Chemistry Transport Model)
+#
+# Copyright (C) 2013 Benoit Bovy
+#
+#
+# see license.txt for more details
+#
+#
+# Last modification: 07/2013
+
+"""
+An simple interface to most of pygchem features (similar to pylab for
+matplotlib and mlab for mayavi).
+
+This module is currently empty (TODO).
+
+"""
+

6bbe8cf75a76e63943b40b975a1cf9e11f145b7c
Benoît Bovy <bbovy@ulg.ac.be>
2014-07-31 16:45:07 +0200

    fixed issue with setup.py develop

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
-
 import os
 from distutils.core import setup
+
+import setuptools
 
 from pygchem import __version__, __project_url__
 
 NAME = 'PyGChem'
 LIBNAME = 'pygchem'
-DATA_DIR = 'data'                   # path relative to LIBNAME
-EXCLUDE_DIRS = ('temp',)            # paths relative to LIBNAME
+DATA_DIR = 'data'  # path relative to LIBNAME
+EXCLUDE_DIRS = ('temp',)  # paths relative to LIBNAME
+
 
 def get_package_data(name, extlist):
     """Return data files for package *name* with extensions in *extlist*"""
     flist = []
     # Workaround to replace os.path.relpath (not available until Python 2.6):
-    offset = len(name)+len(os.pathsep)
+    offset = len(name) + len(os.pathsep)
     for dirpath, _dirnames, filenames in os.walk(name):
         for fname in filenames:
             if (not fname.startswith('.')
@@ -22,6 +24,7 @@ def get_package_data(name, extlist):
                 flist.append(os.path.join(dirpath, fname)[offset:])
     print "Data files to install: %s" % ", ".join(flist)
     return flist
+
 
 def get_subpackages(name):
     """Return subpackages of package *name*"""
@@ -36,25 +39,28 @@ def get_subpackages(name):
     print "Packages and subpackages to install: %s" % ", ".join(splist)
     return splist
 
+
 def get_packages():
     """Return package list"""
     packages = get_subpackages(LIBNAME)  # + get_subpackages('pygchemplugins')
     return packages
 
-setup(name=NAME,
-      version=__version__,
-      description='Python interface to GEOS-Chem 3D Chemistry Transport Model',
-      long_description=open('README.md').read(),
-      keywords='Interface GEOS-Chem Chemistry Transport Model CTM Atmospheric',
-      author='Benoit Bovy',
-      author_email='bbovy@ulg.ac.be',
-      license='GPLv3',
-      url=__project_url__,
-      download_url='%s/archive/%s.tar.gz' % (__project_url__, __version__),
-      platforms=['any'],
-      packages=get_packages(),
-      package_data={LIBNAME: get_package_data(os.path.join(LIBNAME, DATA_DIR),
-                                              ('.*'))},
-      scripts=[],
-      requires=["matplotlib (>= 1.1.0)",
-                "numpy (>= 1.5.0)"])
+
+setup(
+    name=NAME,
+    version=__version__,
+    description='Python interface to GEOS-Chem 3D Chemistry Transport Model',
+    long_description=open('README.md').read(),
+    keywords='Interface GEOS-Chem Chemistry Transport Model CTM Atmospheric',
+    author='Benoit Bovy',
+    author_email='bbovy@ulg.ac.be',
+    license='GPLv3',
+    url=__project_url__,
+    download_url='%s/archive/%s.tar.gz' % (__project_url__, __version__),
+    platforms=['any'],
+    packages=get_packages(),
+    package_data={LIBNAME: get_package_data(os.path.join(LIBNAME, DATA_DIR),
+                                            ('.*'))},
+    scripts=[],
+    requires=["numpy (>= 1.7.0)"]
+)
